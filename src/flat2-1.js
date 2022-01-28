8884cd6cb020eb32522e837af3f2c148caa6b7fb
IvKos81 <78110108+IvKos81@users.noreply.github.com>
2022-01-28 23:21:06 +0300

    Added description to some flats page
    Added animated description to each room in flat. Completed flats 1-1 and 2-1

--- src/flat2-1.js
+++ src/flat2-1.js
@@ -21,3 +21,92 @@ $(window).scroll(function() {
     };
 
 })
+
+// подсветка выбора на изображении
+
+$('#flat2-1').maphilight({
+    fill: true,
+    fillColor: '2252a9',
+    fillOpacity: 0.5,
+    stroke: true,
+    strokeColor: 'ffccaa' 
+});
+
+
+// появление текста при наведении мыши
+
+let coridor = document.querySelector('#coridor');
+let kitchen = document.querySelector('#kitchen');
+let toilet = document.querySelector('#toilet');
+let bathroom = document.querySelector('#bathroom');
+let storageroom = document.querySelector('#storageroom');
+let balcony = document.querySelector('#balcony');
+let bedroom = document.querySelector('#bedroom');
+let livingroom = document.querySelector('#livingroom');
+let flDescr = document.querySelector('.flat_description');
+let flDescrText = document.querySelector('.flat_description_text');
+
+
+coridor.addEventListener('mouseover', (evt) => {
+    flDescr.style.display = 'block'
+    flDescrText.innerHTML = 'Большой коридор, площадью 5 м2. Напольное покрытие - керамогранит. Стены оклеены изностойкими обоями под окраску. Размеры коридора позволяют установить внутри него встроенный шкаф';
+});
+coridor.addEventListener('mouseleave', (evt) => {
+   flDescr.style.display = 'none'
+});
+
+kitchen.addEventListener('mouseover', (evt) => {
+    flDescr.style.display = 'block'
+    flDescrText.innerHTML = 'Светлая кухня, площадью 7м2. Напольное покрытие - керамическая плитка. Стены, по желанию, могут быть отделаны обоями под покраску, кафельной плиткой или декоративной штукатуркой.';
+});
+kitchen.addEventListener('mouseleave', (evt) => {
+   flDescr.style.display = 'none'
+});
+
+toilet.addEventListener('mouseover', (evt) => {
+    flDescr.style.display = 'block'
+    flDescrText.innerHTML = 'Раздельный санузел. Площадь - 1,2 м2. Согласно предлагаемой отделке устанавливается обычный унитаз со сливным бачком. По желанию клиента может быть установлен встроенный унитаз-инсталляция, а также возможно укомплектование гигиеническим душем и маленькой раковиной.';
+});
+toilet.addEventListener('mouseleave', (evt) => {
+   flDescr.style.display = 'none'
+});
+
+bathroom.addEventListener('mouseover', (evt) => {
+    flDescr.style.display = 'block'
+    flDescrText.innerHTML = 'Ванная комната, площадью 2,6 м2. Повышенная гидроизоляция. Отделка - кафельная плитка. Установлена ванная размерами 170х50 см. По желанию заказчика может быть смонтирована душевая кабина. ';
+});
+bathroom.addEventListener('mouseleave', (evt) => {
+   flDescr.style.display = 'none'
+});
+
+storageroom.addEventListener('mouseover', (evt) => {
+    flDescr.style.display = 'block'
+    flDescrText.innerHTML = 'Небольшая кладовая комната, площадью 1,1м2. Может быть использована как комната для установки стиральной машины или как встроенная гардеробная. Напольное покрытие - керамогранит, стены отделаны обоями под окраску. Дополнительно в комнате выведена электрическая розетка.';
+});
+storageroom.addEventListener('mouseleave', (evt) => {
+   flDescr.style.display = 'none'
+});
+
+balcony.addEventListener('mouseover', (evt) => {
+    flDescr.style.display = 'block'
+    flDescrText.innerHTML = 'Большая лоджия на 2 окна. По желанию заказчика может быть установлено холодное или теплое остекление. Дополнительно доступна опция утепления балкона.';
+});
+balcony.addEventListener('mouseleave', (evt) => {
+   flDescr.style.display = 'none'
+});
+
+bedroom.addEventListener('mouseover', (evt) => {
+    flDescr.style.display = 'block'
+    flDescrText.innerHTML = 'Небольшая уютная спальня. Отделка стен -  обои под покраску, напольное покрытие - ламинат 35 класса. Комната может быть переоборудована в детскую. ';
+});
+bedroom.addEventListener('mouseleave', (evt) => {
+   flDescr.style.display = 'none'
+});
+
+livingroom.addEventListener('mouseover', (evt) => {
+    flDescr.style.display = 'block'
+    flDescrText.innerHTML = 'Большая светлая комната, площадью 20м2. Стены отделаны обоями под покраску. Напольное покрытие - ламинат 34 класса. Потолок - оштукатурен и окрашен в три слоя.';
+});
+livingroom.addEventListener('mouseleave', (evt) => {
+    flDescr.style.display = 'none'
+});
